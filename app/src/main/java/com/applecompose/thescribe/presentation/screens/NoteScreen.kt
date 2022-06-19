03e5db05d9c5e8b627566f8d6324d37dbdcce1ed
Jerry applegarth <applegarth1630@gmail.com>
2022-06-19 08:39:20 -0700

    1.4 note setup

--- app/src/main/java/com/applecompose/thescribe/presentation/screens/NoteScreen.kt
+++ app/src/main/java/com/applecompose/thescribe/presentation/screens/NoteScreen.kt
@@ -1,36 +1,58 @@
 package com.applecompose.thescribe.presentation.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.applecompose.thescribe.R
+import com.applecompose.thescribe.presentation.components.NoteInputText
 import com.applecompose.thescribe.presentation.components.TopApp
 import com.applecompose.thescribe.ui.theme.TheScribeTheme
 import com.applecompose.thescribe.ui.theme.newBackgroundColor
 
 @Composable
 fun NoteScreen() {
+	val title by remember { mutableStateOf("")}
+	val description by remember { mutableStateOf("")}
 	Column(
 		modifier = Modifier
 			.padding(6.dp)
 	) {
+
 		TopApp()
 		// Content goes here
+		Column(
+			modifier = Modifier
+				.fillMaxWidth(),
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Spacer(modifier = Modifier.height(6.dp))
+			NoteInputText(
+				text = title,
+				label = "Title",
+				onTextChange = {})
+			Spacer(modifier = Modifier.height(6.dp))
+			NoteInputText(
+				text = description,
+				label = "Add a Note",
+				onTextChange = {})
+
+
+		}
 
 
 
-
-		
 	}
+
+
+
+
 	
 
 }
