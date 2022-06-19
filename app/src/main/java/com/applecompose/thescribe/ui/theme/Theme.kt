df543720d8a63a6e6dc38e37ed7d41f71215fb2c
Jerry applegarth <applegarth1630@gmail.com>
2022-06-19 06:06:01 -0700

    1.1 color and theme setup

--- app/src/main/java/com/applecompose/thescribe/ui/theme/Theme.kt
+++ app/src/main/java/com/applecompose/thescribe/ui/theme/Theme.kt
@@ -5,17 +5,31 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
 private val DarkColorPalette = darkColors(
-	primary = Purple200,
-	primaryVariant = Purple700,
-	secondary = Teal200
+	primary = DarkPrimary,
+	primaryVariant = DarkPrimaryVariant,
+	secondary = DarkSecondary,
+	onPrimary = DarkOnPrimary,
+	onSecondary = DarkOnSecondary,
+	onSurface = DarkOnSurface,
 )
 
 private val LightColorPalette = lightColors(
-	primary = Purple500,
-	primaryVariant = Purple700,
-	secondary = Teal200
+	primary = Primary,
+	primaryVariant = PrimaryVariant,
+	secondary = Secondary,
+	onSurface = OnSurface,
+
+
+
+	/* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+
+
+    */
+)
+
 
 	/* Other default colors to override
     background = Color.White,
@@ -25,7 +39,9 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
-)
+
+
+
 
 @Composable
 fun TheScribeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
